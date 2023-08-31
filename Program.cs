aae8fd512114d0521e1c44f60471baa858715db6
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2023-08-31 21:09:38 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace bubblesort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 3, 2, 6, 7, 5, 9, 8, 1, 12, 10, 11, 4};
+
+        foreach (var num in array)
+        {
+            Console.Write(num + ", ");
+        }
+
+        // Todo:
+        // Write the sorting algorithm
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Bubblesort
+
+School project for sorting an array of number using the bubble sorting algorithm. 
