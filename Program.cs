667fb45fe85631e174fe6add195f7f0c9d1b208f
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-01-06 02:04:10 +0100

    Added more methods for sorting direction to sorting method and added another bubbleSort method for strings.

--- Program.cs
+++ Program.cs
@@ -1,36 +1,195 @@
-﻿namespace bubblesort;
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace bubblesort;
 class Program
 {
-    static void BubbleSort(List<int> input)
+    static void Main(string[] args)
     {
-        int n = input.Count - 1;
+        var methodsRaw = new Hashtable
+        {
+            { "SortNumbers()", new Action<Direction>(SortNumbers) },
+            { "SortStrings()", new Action<Direction>(SortStrings) },
+        };
+
+        var methods = new Hashtable
+        {
+            { 1, "SortNumbers()" },
+            { 2, "SortStrings()" },
+        };
+
+        var options = new Hashtable
+        {
+            { 1, Direction.Asc },
+            { 2, Direction.Desc }
+        };
+
+        int selectedMethod = GetInput("Select method to call:", methods);
+        int selectedOption = GetInput("Select which order to sort array in:", options);
+
+        Action<Direction> action = (Action<Direction>)methodsRaw[methods[selectedMethod]!]!;
+        action((Direction)options[selectedOption]!);
+    }
+
+    static short[] BubbleSort(short[] input, Direction direction)
+    {
+        Func<int, int, bool> compare = GetOrder(direction);
         bool swapped;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < input.Length; i++)
         {
             swapped = false;
-            for (int j = 0; j < n - i; j++)
+            for (int j = 1; j < input.Length - i; j++)
             {
-                if (input[j] > input[j + 1])
+                if (compare(input[j - 1], input[j]))
                 {
-                    (input[j], input[j + 1]) = (input[j + 1], input[j]);
+                    (input[j - 1], input[j]) = (input[j], input[j - 1]);
                     swapped = true;
                 }
             }
             if (!swapped) break;
         }
 
-        for (int i = 0; i < n + 1; i++)
+        return input;
+    }
+    static string[] BubbleSortString(string[] input, Direction direction)
+    {
+        Func<string, string, bool> compare = GetOrderString(direction);
+        bool swapped;
+
+        for (int i = 0; i < input.Length; i++)
         {
-            Console.Write(input[i]);
-            if (i < n) Console.Write(", ");
+            swapped = false;
+            for (int j = 1; j < input.Length - i; j++)
+            {
+                if (compare(input[j - 1], input[j]))
+                {
+                    (input[j - 1], input[j]) = (input[j], input[j - 1]);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+
+        return input;
+    }
+
+
+    enum Direction
+    {
+        Asc,
+        Desc
+    }
+
+    static Func<int, int, bool> GetOrder(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Asc:
+                return (x, y) => x > y;
+            case Direction.Desc:
+                return (x, y) => x < y;
+            default:
+                throw new ArgumentOutOfRangeException("Not a direction!");
+        }
+    }
+    static Func<string, string, bool> GetOrderString(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Asc:
+                return (x, y) => string.Compare(x, y) > 0;
+            case Direction.Desc:
+                return (x, y) => string.Compare(x, y) < 0;
+            default:
+                throw new ArgumentOutOfRangeException("Not a direction!");
         }
     }
 
-    static void Main(string[] args)
+
+    static short[] CreateArray(int length)
     {
-        List<int> sortMe = new() { 45, 84, 735, -7, 90, 420, -6, 9, 0, 257, 5231 };
-        BubbleSort(sortMe);
-        Console.ReadKey();
+        Random random = new();
+        short[] array = new short[length];
+        short min = short.MinValue;
+        short max = short.MaxValue;
+
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = (short)random.Next(min, max);
+        }
+
+        return array;
+    }
+
+
+    static void SortNumbers(Direction direction)
+    {
+        Stopwatch watch = new();
+
+        Console.WriteLine("Creating the array...");
+        short[] array = CreateArray(5_00);
+        Console.WriteLine("Array created!\n");
+
+        Console.WriteLine("Sorting the array...");
+        watch.Start();
+        BubbleSort(array, direction);
+        watch.Stop();
+        Console.WriteLine("Array sorted!\n");
+
+        Console.WriteLine("Displaying the array...");
+        Console.WriteLine(string.Join(", ", array));
+        Console.WriteLine("Displayed the array!\n");
+
+        Console.WriteLine($"Time elapsed: {watch.ElapsedMilliseconds} ms");
+    }
+    static void SortStrings(Direction direction)
+    {
+        Stopwatch watch = new();
+
+        string[] array = new string[] { "Satisfactory", "Space Engineers", "Dyson Sphere Program", "Programming", "Gaming", "Fishing", "Coca Cola", "Word", "Visual Studio Code", "C#" };
+
+        Console.WriteLine("Sorting the array...");
+        watch.Start();
+        BubbleSortString(array, direction);
+        watch.Stop();
+        Console.WriteLine("Array sorted!\n");
+
+        Console.WriteLine("Displaying the array...");
+        Console.WriteLine(string.Join(", ", array));
+        Console.WriteLine("Displayed the array!\n");
+
+        Console.WriteLine($"Time elapsed: {watch.ElapsedMilliseconds} ms");
+    }
+
+
+    static int GetInput(string question, Hashtable options)
+    {
+        int input;
+        bool ok;
+        do
+        {
+            Console.WriteLine(question);
+
+            for (int i = 1; i <= options.Count; i++)
+            {
+                Console.WriteLine("Option " + i + ": " + options[i]);
+            }
+
+            Console.Write("Write your option: ");
+
+            ok = int.TryParse(Console.ReadLine(), out input);
+            if (!ok) Console.WriteLine("You can only input a number!\n");
+
+            if (input > options.Count || input < 1)
+            {
+                ok = false;
+                Console.WriteLine("Selected option does not exist, try again.\n");
+            }
+
+        } while (!ok);
+
+        Console.WriteLine($"You selected: {options[input]}\n");
+        return input;
     }
 }
